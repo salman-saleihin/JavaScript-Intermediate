5862c5ebd37009966188925b9ef34e5e597c646f
salman-saleihin <saleihinkhan@gmail.com>
2022-12-20 19:03:44 +0600

    All About Call,Apply and Bind in Detail

--- callApply.js
+++ callApply.js
@@ -7,17 +7,17 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount , tips , tax){
         // console.log(this);
-        this.salary = this.salary - amount ;
+        this.salary = this.salary - amount - tips - tax ;
         return this.salary ;
     } 
 }
 // console.log(normalPerson);
-    normalPerson.chargeBill(150);
-    normalPerson.chargeBill(850);
-    normalPerson.chargeBill(400);
-// console.log(normalPerson.salary);
+    normalPerson.chargeBill(150 ,0 , 0);
+    normalPerson.chargeBill(850 , 0 , 0);
+    normalPerson.chargeBill(400 ,0 ,10);
+ console.log("Normal Person Salary Remain :" , normalPerson.salary);
 
 // Now LETS Start Bind Related Details and Code :: (object use bind to borrow method from another object)
 
@@ -32,13 +32,35 @@ const friendlyPerson = {
     lastName : 'Ubydha',
     salary : 18000 ,
  }
+
+ const businessPerson = {
+    firstName : 'Abtahii' ,
+    lastName : 'Sameen',
+    salary : 50000 ,
+ }
 //  normalPerson.chargeBill();
- const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
- heroChargeBill(2000);
- heroChargeBill(3000);
- console.log("Hero Person Salary Remain = " ,heroPerson.salary);
- console.log("Normal person Salary Remain = " ,normalPerson.salary);
+
+//  const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+//  heroChargeBill(2000);
+//  heroChargeBill(3000);
+//  console.log("Hero Person Salary Remain = " ,heroPerson.salary);
+//  console.log("Normal person Salary Remain = " ,normalPerson.salary);
 
  const friendlyPersonChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
- friendlyPersonChargeBill(500);
- console.log("Friendly Person Salary Remain = " , friendlyPerson.salary);
+ friendlyPersonChargeBill(500 , 50 ,60);
+ console.log("Friendly Person Salary Remain using Bind = " , friendlyPerson.salary);
+
+// Now LETS DISCUSS difference between bind, call and apply ::
+
+normalPerson.chargeBill.call(heroPerson, 900, 50 , 50 ); 
+normalPerson.chargeBill.call(heroPerson, 1000, 30 , 20 ); 
+console.log("Remaining Salary of HeroPerson Using Call :" ,heroPerson.salary);
+
+normalPerson.chargeBill.call(friendlyPerson, 10,10,10);
+console.log("Remaining Salary of FriendlyPerson Using Call :" , friendlyPerson.salary);
+
+// LETS TRY Same Thing with APPLY! :
+
+normalPerson.chargeBill.apply(businessPerson, [4000 ,500 ,500]);
+console.log("Remaining Salary of BusinessPerson using Apply :" , businessPerson.salary);
+
