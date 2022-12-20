8b827bc5e8079dc0efe6db1abac088972fee48b4
salman-saleihin <saleihinkhan@gmail.com>
2022-12-20 14:55:09 +0600

    first class done

---
+++ callApply.js
@@ -0,0 +1,19 @@
+const normalPerson = {
+    firstName : 'Sharaban',
+    lastName : 'Tohura' ,
+    salary : 15000 ,
+    getFullName : function(){
+        console.log(this.firstName , this.lastName);
+    },
+    chargeBill : function(amount){
+        this.salary = this.salary - amount ;
+        return this.salary ;
+    } 
+}
+
+// console.log(normalPerson);
+
+normalPerson.chargeBill(150);
+normalPerson.chargeBill(850);
+normalPerson.chargeBill(4000);
+console.log(normalPerson.salary);
