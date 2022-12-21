d5de04f71b7c2b0abfc02c99d792a519b9448f3f
salman-saleihin <saleihinkhan@gmail.com>
2022-12-21 20:00:03 +0600

    Create similar pattern Object with class,constructor,new Keyword!

---
+++ new_keyword.js
@@ -0,0 +1,15 @@
+// Jokhon onek gula object lagbe similar Style e or same pattern e tokhon ::
+
+class person {
+    constructor(firstName , lastName , salary){
+        this.firstName = firstName ;
+        this.lastName = lastName ;
+        this.salary = salary ;
+    }
+}
+
+const heroPerson = new person('Salman' , 'Muqtadeer' , 35000);
+console.log(heroPerson);
+
+const friendlyPerson = new person('Sharzil' , 'Kabir' , 20000);
+console.log(friendlyPerson);
